8a391626c6cd470573f0a99879af79a038f64ebd
Alex Holub <alexholub@microsoft.com>
2025-07-26 21:57:21 -0700

    Added endpoint setup

---
+++ src/IdentityProvider.Api/Endpoints/Weatherforecast.cs
@@ -0,0 +1,30 @@
+﻿using MinimalEndpoints.Abstractions;
+
+namespace IdentityProvider.Api.Endpoints;
+
+internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
+
+public class Weatherforecast : IEndpoint
+{
+    private readonly static string[] _summaries = ["Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"];
+
+    public void MapEndpoint(IEndpointRouteBuilder app)
+    {
+        app.MapGet("weatherforecast", Handle);
+    }
+
+    private static WeatherForecast[] Handle()
+    {
+        var forecast = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            _summaries[Random.Shared.Next(_summaries.Length)]
+        )).ToArray();
+
+        return forecast;
+    }
+}
--- src/IdentityProvider.Api/Program.cs
+++ src/IdentityProvider.Api/Program.cs
@@ -1,4 +1,35 @@
+using Asp.Versioning;
+using Asp.Versioning.Builder;
+using MinimalEndpoints.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddApiVersioning(options =>
+{
+    options.DefaultApiVersion = new ApiVersion(1);
+    options.AssumeDefaultVersionWhenUnspecified = true;
+    options.ApiVersionReader = ApiVersionReader.Combine(
+        new UrlSegmentApiVersionReader(),
+        new QueryStringApiVersionReader(),
+        new HeaderApiVersionReader("X-Version"),
+        new MediaTypeApiVersionReader("ver")
+    );
+}).AddApiExplorer(options =>
+{
+    options.GroupNameFormat = "'v'VVV";
+    options.SubstituteApiVersionInUrl = true;
+});
+
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Title = "Identity Provider API",
+        Version = "v1",
+    });
+});
+
+builder.Services.AddEndpoints(typeof(Program).Assembly);
 
 // Add services to the container.
 
@@ -6,29 +37,23 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
+ApiVersionSet apiVersionSet = app.NewApiVersionSet()
+    .HasApiVersion(new ApiVersion(1))
+    .ReportApiVersions()
+    .Build();
+
+RouteGroupBuilder versionedGroup = app
+    .MapGroup("api/v{version:apiVersion}")
+    .WithApiVersionSet(apiVersionSet);
+
+app.MapEndpoints(versionedGroup);
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-});
-
 app.Run();
-
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
